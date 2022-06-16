f9267277ff481cf8754f38d46ea2c10276656263
Callum <67241029+Caallum@users.noreply.github.com>
2022-06-16 18:44:43 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,28 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const session = require("express-session");
 
+const OAuthRouter = require("./router/oauth.js")
+
+app.use(cors({
+    credentials: true
+}));
+app.use(
+    session({
+        resave: false,
+        saveUninitialized: false,
+        secret: "keyboard cat"
+    })
+)
+
+app.get("/api/status", (req, res) => {
+    res.json({
+        status: "good",
+        message: "Server is working and online"
+    })
+})
+
+app.use("/api/user", OAuthRouter)
+
+app.listen(4080, () => { console.log(`[BACKEND] Running on PORT 4080`)});
