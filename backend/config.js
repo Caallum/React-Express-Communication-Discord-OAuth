a567e0b714184514bd309c96ffda0b7bc8621527
Callum <67241029+Caallum@users.noreply.github.com>
2022-06-16 18:40:52 +0100

    Create config.js

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    redirectUri: "<YOUR BACKEND URL>/api/user/auth",
+    clientId: "<YOUR CLIENT ID>",
+    clientSecret: "<YOUR CLIENT SECRET>",
+    frontURL: "<YOUR FRONTEND URL>"
+}
