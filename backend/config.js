2421e6428241ca8a4c80bdfc443d2aeb9c623fef
Callum <67241029+Caallum@users.noreply.github.com>
2022-06-16 18:41:15 +0100

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     redirectUri: "<YOUR BACKEND URL>/api/user/auth",
+    oauthUri: "<YOUR OAUTH URL>",
     clientId: "<YOUR CLIENT ID>",
     clientSecret: "<YOUR CLIENT SECRET>",
     frontURL: "<YOUR FRONTEND URL>"
