b7d2f61703d3e1021d40913c2129efd2cb35bfb8
Callum <67241029+Caallum@users.noreply.github.com>
2022-06-16 18:41:31 +0100

    Update TokenHandler.js

--- backend/functions/TokenHandler.js
+++ backend/functions/TokenHandler.js
@@ -1,1 +1,27 @@
+const axios = require("axios");
+const config = require("../config")
 
+async function TokenHandler(code) {
+    if(!code) throw new TypeError("Cannot read 'code'")
+
+    const data = new URLSearchParams();
+    data.append("client_id", config.clientId);
+    data.append("client_secret", config.clientSecret);
+    data.append("redirect_uri", config.redirectUri);
+    data.append("scopes", "identify");
+    data.append("grant_type", "authorization_code");
+    data.append("code", code);
+
+    const { data: Token } = await axios({
+        method: "POST",
+        url: "https://discord.com/api/oauth2/token",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        data: data
+    });
+
+    return Token;
+}
+
+module.exports = TokenHandler;
